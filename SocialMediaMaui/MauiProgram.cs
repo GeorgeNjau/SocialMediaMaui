a1b2e7763b71afaac6698ce1a2996ff74af0b2ff
George Mission <gkmission@outlook.com>
2025-01-23 08:04:36 +0300

    Refactor constructors and update UI elements
    - Reformatted constructor methods in various files for consistent indentation.
    - Removed `MainPage.xaml` and `MainPage.xaml.cs`, replaced references with `HomePage` in `AppShell.xaml`.
    - Updated `MauiProgram.cs` to include `CommunityToolkit.Maui` package and use `Ubuntu` fonts.
    - Reformatted `MainActivity.cs` and `Main.cs` for better readability.
    - Changed `colorPrimaryDark` in `colors.xml` from `#2B0B98` to `#de1c80`.
    - Updated color definitions in `Colors.xaml` and font family in `Styles.xaml` to `UbuntuRegular`.
    - Made minor formatting changes and updated `PackageReference` elements in `SocialMediaMaui.csproj`.
    - Added new folders (`Models`, `Controls`, `Templates`, `ViewModels`) and set `HomePage.xaml.cs` as dependent on `HomePage.xaml`.

--- SocialMediaMaui/MauiProgram.cs
+++ SocialMediaMaui/MauiProgram.cs
@@ -1,24 +1,26 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace SocialMediaMaui;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
+                fonts.AddFont("Ubuntu-Regular.ttf", "UbuntuRegular");
+            })
+            .UseMauiCommunityToolkit();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
