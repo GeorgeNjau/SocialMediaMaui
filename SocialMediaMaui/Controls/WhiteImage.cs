b2c8f56050e4b9b87fe01df97dde9bcd27042506
George Mission <gkmission@outlook.com>
2025-01-24 23:59:27 +0300

    Update solution, HomePage, and add WhiteImage control
    Updated SocialMediaMaui.sln to include Debug deployment settings and a new SolutionProperties section. Modified HomePage.xaml to add new XML namespaces, replace ScrollView and VerticalStackLayout with Shell.TitleView, and include a Grid with title and icons using WhiteImage control. Removed OnCounterClicked event handler from HomePage.xaml.cs. Added WhiteImage.cs to define a custom WhiteImage control with white tint using IconTintColorBehavior from CommunityToolkit.Maui.

---
+++ SocialMediaMaui/Controls/WhiteImage.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaMaui.Controls
+{
+    public class WhiteImage : Image
+    {
+        public WhiteImage()
+        {
+            var tintColorBehavior = Behaviors.FirstOrDefault(c => c is IconTintColorBehavior);
+
+            if (tintColorBehavior == null)
+            {
+                tintColorBehavior = new IconTintColorBehavior
+                {
+                    TintColor = Colors.White
+                };
+
+                Behaviors.Add(tintColorBehavior);
+            }
+        }
+    }
+}
